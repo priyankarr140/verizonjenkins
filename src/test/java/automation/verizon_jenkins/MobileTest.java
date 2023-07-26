86622c0a08e111f3752b6b339f12a031b102fddc
Priyanka <priyankarr140@gmail.com>
2023-07-26 14:08:36 +0530

    initial commit

---
+++ src/test/java/automation/verizon_jenkins/MobileTest.java
@@ -0,0 +1,18 @@
+package automation.verizon_jenkins;
+
+import org.testng.annotations.Test;
+
+public class MobileTest {
+
+	@Test
+	void login()
+	{
+		System.out.println("Login");
+	}
+	@Test
+	void logout()
+	{
+		System.out.println("Logout");
+		//int num=10/0;
+	}
+}
